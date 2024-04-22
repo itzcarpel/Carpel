d140303bfe55ebf0b954e2ad8231ae730dd47349
Carpel <62513059+itzcarpel@users.noreply.github.com>
2024-04-22 10:37:12 +0200

    Update sys32.ps1

--- scripts/sys32.ps1
+++ scripts/sys32.ps1
@@ -2,7 +2,7 @@ Write-Host @"
 [38;2;160;32;240;48;2m  _   _                     _____ ____ ___  
  | \ | |                   / ____|___ \__ \ 
  |  \| | __ _ _ __ ___ ___| (___   __) | ) |
- |  . ` |/ _` | '__/ __/ _ \\___ \ |__ < / / 
+ | . ` |/ _` | '__/ __/ _ \\___ \ |__ < / / 
  | |\  | (_| | | | (_| (_) |___) |___) / /_ 
  |_| \_|\__,_|_|  \___\___/_____/|____/_____[0m
                                             
@@ -21,9 +21,15 @@ foreach ($file in $files) {
     # Check if the file is an executable
     if ($file.Extension -eq ".exe") {
         # Check if the file is signed
-        $signature = Get-AuthenticodeSignature $file.FullName -ErrorAction SilentlyContinue
-        if (-not $signature) {
-            Write-Host "Unsigned: $($file.FullName)"
+        try {
+            $signature = Get-AuthenticodeSignature $file.FullName -ErrorAction Stop
+            if (-not $signature) {
+                Write-Host "Unsigned: $($file.FullName)"
+            }
+        } catch {
+            if ($_.Exception.GetType().FullName -eq 'System.Security.Cryptography.CryptographicException') {
+                Write-Host "Unsigned: $($file.FullName)"
+            }
         }
     }
 }
