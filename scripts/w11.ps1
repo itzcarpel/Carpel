cc6519d0113961ec5995e36327d04b9793dd1740
Carpel <62513059+itzcarpel@users.noreply.github.com>
2024-04-23 11:40:35 +0200

    Update w11.ps1

--- scripts/w11.ps1
+++ scripts/w11.ps1
@@ -1,10 +1,7 @@
 $inputFile = "C:\Windows\appcompat\pca\PcaGeneralDb0.txt"
 
-function IsSuspicious($productname, $publisher, $productversion, $exitcode) {
-    $isNull = [string]::IsNullOrEmpty($productname) -and [string]::IsNullOrEmpty($publisher) -and [string]::IsNullOrEmpty($productversion)
-    $isInstallerFailed = $exitcode -like "Installer failed"
-    $isKernelDriver = $exitcode -like "*KernelDriver*"
-    return ($isNull -and ($isInstallerFailed -or $isKernelDriver))
+function IsSuspicious($productname, $publisher, $productversion) {
+    return [string]::IsNullOrEmpty($productname) -and [string]::IsNullOrEmpty($publisher) -and [string]::IsNullOrEmpty($productversion)
 }
 
 function ParseLine($line) {
@@ -12,7 +9,7 @@ function ParseLine($line) {
     $productname = if ($fields[3] -eq '') { $null } else { $fields[3] }
     $publisher = if ($fields[4] -eq '') { $null } else { $fields[4] }
     $productversion = if ($fields[5] -eq '') { $null } else { $fields[5] }
-    $suspicious = IsSuspicious $productname $publisher $productversion $fields[7]
+    $suspicious = IsSuspicious $productname $publisher $productversion
     return [PSCustomObject]@{
         Time = $fields[0]
         Runcount = $fields[1]
@@ -33,7 +30,7 @@ Write-Host "";
 Write-Host -ForegroundColor Red "  _   _                 __          ____ __ ";
 Write-Host -ForegroundColor Red " | \ | |                \ \        / /_ /_ |";
 Write-Host -ForegroundColor Red " |  \| | __ _ _ __ ___ __\ \  /\  / / | || |";
-Write-Host -ForegroundColor Red " | .  ` |/ _` | '__/ __/ _ \ \/  \/ /  | || |";
+Write-Host -ForegroundColor Red " | . ` |/ _` | '__/ __/ _ \ \/  \/ /  | || |";
 Write-Host -ForegroundColor Red " | |\  | (_| | | | (_| (_) \  /\  /   | || |";
 Write-Host -ForegroundColor Red " |_| \_|\__,_|_|  \___\___/ \/  \/    |_||_|";
 Write-Host "";
